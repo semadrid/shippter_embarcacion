7eb624d5b93e6076325fc15390cf6018208eeff6
Sebastián Madrid <seba.32@gmail.com>
2023-02-24 22:38:18 -0300

    Se agrega modal Update

---
+++ shippter_test/src/components/ModalForm.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import moment from "moment";
+
+const ModalForm = ({ info, visible, setVisible, handleSubmit }) => {
+  const handleOnClick = () => {
+    setVisible(!visible);
+  };
+  const [ship, setShip] = useState({});
+  const handleClose = () => setVisible(false);
+
+  const handleChange = (e) => {
+    setShip({
+      ...ship,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnClickSubmit = () => {
+    handleSubmit(ship);
+  };
+
+  console.log("ship", ship);
+  useEffect(() => {
+    setShip(info);
+
+    return () => {
+      setShip({});
+    };
+  }, [info]);
+
+  if (!visible) return null;
+  return (
+    <div className={`modal ${visible ? "" : "off"}`} id="modal">
+      <Button variant="primary" onClick={setVisible}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={visible} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Actualizar {ship.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="content">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Nombre
+                </label>
+                <input
+                  value={ship.name}
+                  name="name"
+                  onChange={handleChange}
+                  type="Text"
+                  id="name"
+                  className="form-control"
+                  placeholder="Nombre de la embarcación"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="pos_x" className="form-label">
+                  Pos X
+                </label>
+                <input
+                  value={ship.pos_x}
+                  name="pos_x"
+                  onChange={handleChange}
+                  type="number"
+                  id="pos_x"
+                  className="form-control"
+                  placeholder="Número de posición X"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="pos_y" className="form-label">
+                  Pos Y
+                </label>
+                <input
+                  value={ship.pos_y}
+                  name="pos_y"
+                  onChange={handleChange}
+                  type="number"
+                  id="pos_y"
+                  className="form-control"
+                  placeholder="Número de posición Y"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="pos_z" className="form-label">
+                  Pos Z
+                </label>
+                <input
+                  value={ship.pos_z}
+                  name="pos_z"
+                  onChange={handleChange}
+                  type="number"
+                  id="pos_z"
+                  className="form-control"
+                  placeholder="Número de posición Z"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="destiny" className="form-label">
+                  Destino
+                </label>
+                <input
+                  value={ship.destiny}
+                  name="destiny"
+                  onChange={handleChange}
+                  type="Text"
+                  id="destiny"
+                  className="form-control"
+                  placeholder="Destino de la embarcación"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="pointing_at" className="form-label">
+                  Apuntando
+                </label>
+                <input
+                  value={ship.pointing_at}
+                  name="pointing_at"
+                  onChange={handleChange}
+                  type="Text"
+                  id="pointing_at"
+                  className="form-control"
+                  placeholder="Si apunta a Norte/Sur/Este/Oeste"
+                ></input>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="sail_date" className="form-label">
+                  Fecha de Salida
+                </label>
+                <input
+                  value={moment.utc(ship.sail_date).format("yyyy-MM-DD")}
+                  name="sail_date"
+                  onChange={handleChange}
+                  type="Date"
+                  id="sail_date"
+                  className="form-control"
+                ></input>
+              </div>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleOnClickSubmit}>
+            Actualizar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+ModalForm.defaultProps = {
+  handleSubmit: () => {},
+};
+export default ModalForm;
--- shippter_test/src/components/ShipList.js
+++ shippter_test/src/components/ShipList.js
@@ -1,55 +1,116 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
 import moment from "moment";
-
+import ModalForm from "./ModalForm";
 
 //tabla para ships TO DO editar y borrar
-const ShipList = ({ships, ship, setListUpdated}) => {
+const ShipList = ({ ships, ship, setListUpdated }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalInfo, setModalInfo] = useState({
+    name: "",
+    pos_x: 0,
+    pos_y: 0,
+    pos_z: 0,
+    destiny: "",
+    pointing_at: "",
+    sail_date: "",
+  });
+  const handleDelete = (id) => {
+    const requestInit = {
+      method: "DELETE",
+    };
+    fetch("http://localhost:8000/api/ships/" + id, requestInit)
+      .then((res) => res.text())
+      .then((res) => console.log(res));
 
-    const handleDelete = id => {
-        const requestInit = {
-            method: 'DELETE'
-        }
-        fetch('http://localhost:8000/api/ships/' + id, requestInit)
-		.then(res => res.text())
-		.then(res => console.log(res))
-
-        setListUpdated(true)
+    setListUpdated(true);
+  };
+  const handleOnClickUpdate = (ship) => {
+    setShowModal(true);
+    setModalInfo(ship);
+  };
+  const handleSubmit = (ship) => {
+    ship.pos_x = parseFloat(ship.pos_x);
+    ship.pos_y = parseFloat(ship.pos_y);
+    ship.pos_z = parseFloat(ship.pos_z);
+    //validación campos vacíos
+    if (
+      ship.name === "" ||
+      ship.pos_x === null ||
+      ship.pos_y === null ||
+      ship.pos_z === null ||
+      ship.destiny === "" ||
+      ship.pointing_at === "" ||
+      ship.sail_date === ""
+    ) {
+      alert("Los campos deben estar completados correctamente");
+      return;
     }
+    const requestInit = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ship),
+    };
+    fetch("http://localhost:8000/api/ships/" + ship._id, requestInit)
+      .then((res) => res.text())
+      .then((res) => console.log(res));
 
-    return (
-        <table className='table'>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Pos X</th>
-                    <th>Pos Y</th>
-                    <th>Pos Z</th>
-                    <th>Destino</th>
-                    <th>Apuntando</th>
-                    <th>Fecha de Salida</th>
-                </tr>
-            </thead>
-            <tbody>
-                {ships.map(ship => (
-                    <tr key={(ship._id)}>
-                        <td>{ship.name}</td>
-                        <td>{ship.pos_x}</td>
-                        <td>{ship.pos_y}</td>
-                        <td>{ship.pos_z}</td>
-                        <td>{ship.destiny}</td>
-                        <td>{ship.pointing_at}</td>
-                        <td>{moment.utc(ship.sail_date).format("D/MM/YYYY")}</td>
-                        <td>
-                            <div className='mb-3'>
-                                <button onClick={() => handleDelete(ship._id)} className='btn btn-danger'>Borrar</button>
-                            </div>
-                        </td>
-                    </tr>
+    setShowModal(false);
+    setListUpdated(true);
+  };
 
-                ))}
-            </tbody>
-        </table>
-    )
-} 
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Pos X</th>
+            <th>Pos Y</th>
+            <th>Pos Z</th>
+            <th>Destino</th>
+            <th>Apuntando</th>
+            <th>Fecha de Salida</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ships.map((ship) => (
+            <tr key={ship._id}>
+              <td>{ship.name}</td>
+              <td>{ship.pos_x}</td>
+              <td>{ship.pos_y}</td>
+              <td>{ship.pos_z}</td>
+              <td>{ship.destiny}</td>
+              <td>{ship.pointing_at}</td>
+              <td>{moment.utc(ship.sail_date).format("D/MM/YYYY")}</td>
+              <td>
+                <div className="mb-3">
+                  <button
+                    onClick={() => handleDelete(ship._id)}
+                    className="btn btn-danger"
+                  >
+                    Borrar
+                  </button>
+                  <button
+                    onClick={() => handleOnClickUpdate(ship)}
+                    className="btn btn-danger"
+                  >
+                    Actualizar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <ModalForm
+        info={modalInfo}
+        visible={showModal}
+        setVisible={setShowModal}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+};
 
 export default ShipList;
